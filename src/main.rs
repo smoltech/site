fd91666fa839e3c7c4d15ab5f331bb82e7c824bf
Ricardo Batista <emacs.copo@gmail.com>
2022-06-11 08:55:31 +0100

    Tera templates

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use rocket_dyn_templates::{context, Template};
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("home/index", context! {})
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
