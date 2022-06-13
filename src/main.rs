8ef001dd85840d8b9e5839ff5382173941cbff47
Ricardo Batista <emacs.copo@gmail.com>
2022-06-13 15:08:31 +0100

    Add file server for static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+
+use rocket::fs::{relative, FileServer};
 use rocket_dyn_templates::{context, Template};
 
 #[macro_use]
@@ -10,7 +13,21 @@ fn index() -> Template {
 
 #[launch]
 fn rocket() -> _ {
+    let path = get_or_else(
+        String::from("STATICS_PATH"),
+        String::from(relative!("/static")),
+    );
+    let statics = FileServer::from(path);
+
     rocket::build()
         .mount("/", routes![index])
+        .mount("/static", statics)
         .attach(Template::fairing())
+}
+
+fn get_or_else(key: String, default: String) -> String {
+    match env::var(key) {
+        Ok(value) => value,
+        Err(_) => default,
+    }
 }
